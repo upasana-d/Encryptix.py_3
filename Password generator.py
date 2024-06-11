3289a7fbfad3cccb85e893a40a9d5c598d5fee52
upasana-d <upasanadas004@gmail.com>
2024-06-11 14:36:03 +0530

    Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,23 +1,39 @@
 import string
 import random
 
-def gen_pw(length):
-    chars = string.ascii_letters + string.digits + string.punctuation
+def gen_pw(length,complexity):
+    if complexity == "weak":
+        chars = string.ascii_letters 
+    elif complexity == "medium":
+        chars = string.ascii_letters + string.digits 
+    elif complexity == "strong":
+        chars = string.ascii_letters + string.digits + string.punctuation
+    else:
+        raise ValueError("Invalid complexity level")
+        
     password = ''.join(random.choice(chars) for i in range(length))
     return password
 
 def main():
-    try:
-        length = int(input("Enter the desired length of the password:"))
-        if length <= 0:
-            raise ValueError("Password length should be greater than 0.")
-    except ValueError as ve:
-        print("Invalid input:",ve)
-        return
-    
-    password = gen_pw(length)
+    while True:
+        try:
+            length = int(input("Enter the desired length of the password:"))
+            if length <= 0:
+                raise ValueError("Password length should be greater than 0.")
 
-    print("Generated Password : ",password)
+            complexity = input("Enter the complexity level (weak, medium, strong) : ").lower()
+            if complexity not in ["weak", "medium", "strong"]:
+                raise ValueError("Complexity level should be 'weak', 'medium', or 'strong'.")
+                
+        except ValueError as ve:
+            print("Invalid input:",ve)
+            return
+            
+        password = gen_pw(length,complexity)
+        print("Generated Password : ",password)
+        ask = input("Do you want another password> (Y/N) : ").lower()
+        if ask != 'y':
+            break
 
 if __name__ == "__main__":
     main()
